ac20eda7f6a1b37d0fef4cfabf4aee7b9d7ff6d4
Bruthalles <thallesgmkr@gmail.com>
2024-03-18 14:49:05 -0300

    iniciando o passo a passo

--- app.js.js
+++ app.js.js
@@ -1,3 +1,27 @@
+/*Passo a passo do projeto
+
+Ideias: 
+1 - Aplicativo para Academia ...
+2 - Aplicativo de venda (depósito de bebidas)
+
+Objetivo:
+
+====================
+
+Conteúdo plano 1:
+
+Conteúdo plano 2:
+  Produtos
+    Edição dos itens apenas para o vendedor
+    Se possível, uma função que permite escolher produtos em promoção ou em falta
+
+  Chat direto com o vendedor ou APi para o whatsapp
+
+  Notificar atualizações e mensagens
+
+
+*/
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View , Image , Button} from 'react-native';
 import { useState } from 'react';
